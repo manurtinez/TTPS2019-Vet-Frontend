3d1862f6ca1d06f0d4dfcdf84aad0295f91e5b3b
manurtinez <mantinez@gmail.com>
2020-04-04 21:40:43 -0300

    sigo con el modificar mascota

--- src/app/modificar-mascota/modificar-mascota.component.ts
+++ src/app/modificar-mascota/modificar-mascota.component.ts
@@ -1,22 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterContentInit } from '@angular/core';
 import { Mascota } from '../models/mascota';
-import { ActivatedRoute, Router, ParamMap } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { MascotaService } from '../services/mascota-service';
-import { switchMap } from 'rxjs/operators';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-modificar-mascota',
   templateUrl: './modificar-mascota.component.html',
   styleUrls: ['./modificar-mascota.component.css']
 })
-export class ModificarMascotaComponent implements OnInit {
-  mascota: Mascota;
+export class ModificarMascotaComponent implements OnInit, AfterContentInit {
+  public mascota: Mascota;
+  formMascota: FormGroup;
 
   constructor(
     private route: ActivatedRoute,
-    private router: Router,
-    private mascotaService: MascotaService
-  ) {}
+    private mascotaService: MascotaService,
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
     this.mascotaService
@@ -25,10 +26,48 @@ export class ModificarMascotaComponent implements OnInit {
       .subscribe(
         data => {
           this.mascota = data;
+          let fecha = new Date(this.mascota.nacimiento);
+          this.formMascota = this.formBuilder.group({
+            nombre: [this.mascota.nombre, Validators.required],
+            color: [this.mascota.color, Validators.required],
+            especie: [this.mascota.especie, Validators.required],
+            nacimiento: [fecha, Validators.required],
+            sexo: [this.mascota.sexo, Validators.required],
+            senas: [this.mascota.senas, Validators.required],
+            raza: [this.mascota.raza, Validators.required],
+            checkNombre: [null],
+            checkColor: [null],
+            checkEspecie: [null],
+            checkNaci: [null],
+            checkSexo: [null],
+            checkRaza: [null],
+            checkSenas: [null],
+          });
         },
         error => {
           console.log(error);
         }
       );
+    console.log(this.mascota);
+    // this.formMascota = this.formBuilder.group({
+    //   nombre: ['hola', Validators.required],
+    //   color: [null, Validators.required],
+    //   especie: [null, Validators.required],
+    //   nacimiento: [null, Validators.required],
+    //   sexo: [null, Validators.required],
+    //   senas: [null, Validators.required],
+    //   raza: [null, Validators.required],
+    //   checkNombre: [null],
+    //   checkColor: [null],
+    //   checkEspecie: [null],
+    //   checkNaci: [null],
+    //   checkSexo: [null],
+    //   checkRaza: [null],
+    //   checkSenas: [null],
+    // });
+  }
+
+  ngAfterContentInit() {
+
   }
 }
